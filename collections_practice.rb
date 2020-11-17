85f9cae28f91cfa05f6f8c1d7c859c1b9c8d723c
Ben Altman <altmanguitar@gmail.com>
2020-11-17 00:38:51 +0000

    Automatically backed up by Learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -1,1 +1,42 @@
 
+def sort_array_asc(array)
+  array.sort
+end
+
+def sort_array_desc(array)
+  array.sort.reverse
+end
+
+def sort_array_char_count(array)
+  array.sort {|x,y| x.length<=>y.length}
+end
+
+def swap_elements(array)
+  array[1], array[2] = array[2], array[1]
+  array
+end
+
+def reverse_array(array)
+  array.reverse
+end
+
+=begin
+def sort_array_asc(array)
+  array.sort do |x,y|
+    if x==y
+      0
+    elsif x<y
+      -1
+    elsif x>y
+      1
+    end
+  end
+end
+
+def sort_array_desc(array)
+  array.sort do |x,y|
+    y<=>x
+  end
+end
+
+=end
